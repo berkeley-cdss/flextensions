db62bbffa0f8e84b12ed32a5a744b479f197cd1f
Cynthia Li <cynthia.lixinyi@berkeley.edu>
2024-03-13 15:42:57 -0700

    usertocourses succesfully created and migrated; need to fix an error during extensions migration; coursetolmss failed to migrate due to naming issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_13_024435) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_13_222606) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,4 +39,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_13_024435) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "usertocourses", force: :cascade do |t|
+    t.bigint "course_id"
+    t.bigint "user_id"
+    t.string "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_usertocourses_on_course_id"
+    t.index ["user_id"], name: "index_usertocourses_on_user_id"
+  end
+
+  add_foreign_key "usertocourses", "courses"
+  add_foreign_key "usertocourses", "users"
 end
