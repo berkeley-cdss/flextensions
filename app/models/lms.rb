eddf2f1bf07a80cbbd02d69739db5edfd6abd3ae
Michael Ball <michael@mball.co>
2025-11-11 12:37:41 -0800

    Run migrations, fixup initializer

--- app/models/lms.rb
+++ app/models/lms.rb
@@ -4,7 +4,6 @@
 #
 #  id             :bigint           not null, primary key
 #  lms_name       :string
-#  lms_base_url   :string
 #  use_auth_token :boolean
 #  created_at     :datetime         not null
 #  updated_at     :datetime         not null
--- config/initializers/lms_integrations.rb
+++ config/initializers/lms_integrations.rb
@@ -1,16 +1,16 @@
 # Ensure all LMS integrations are set up
-
+# TODO: We should deprecate the need for specific LMSs.
 Rails.application.config.after_initialize do
   CANVAS_LMS_ID = 1
   GRADESCOPE_LMS_ID = 2
 
-  begin
-    next unless ActiveRecord::Base.connection.table_exists?('lmss')
-    # Warm the tiny cache.
-    Lms.CANVAS_LMS
-    Lms.GRADESCOPE_LMS
-  rescue ActiveRecord::NoDatabaseError
-    # Skip if database doesn't exist
-    next
-  end
+  # Disabled because this initialzer is loaded when doing migrations
+  # this casues if the `lmss` table changes.
+  # begin
+  #   next unless ActiveRecord::Base.connection.table_exists?('lmss')
+  #   # Warm the tiny cache.
+  # rescue ActiveRecord::NoDatabaseError
+  #   # Skip if database doesn't exist
+  #   next
+  # end
 end
